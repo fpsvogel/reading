2c8c1f09a008a6fa2d0979c06e90f14197717ee2
Felipe Vogel <fps.vogel@gmail.com>
2022-05-06 11:51:56 -0400

    Faster dig! implementation

--- lib/reading/util/dig_bang.rb
+++ lib/reading/util/dig_bang.rb
@@ -1,21 +1,30 @@
 module Reading
   module Util
-    # Modified from https://github.com/dogweather/digbang
-    def self.deep_fetch(fetchable, keys)
-      keys.reduce(fetchable) { |a, e| a.fetch(e) }
+    module DeepFetch
+      def dig!(*keys)
+        case keys.length
+        when 1
+          fetch(keys[0])
+        when 2
+          fetch(keys[0]).fetch(keys[1])
+        when 3
+          fetch(keys[0]).fetch(keys[1]).fetch(keys[2])
+        when 4
+          fetch(keys[0]).fetch(keys[1]).fetch(keys[2]).fetch(keys[3])
+        end
+        # Or this, more flexible but slightly slower.
+        # From https://github.com/dogweather/digbang
+        # keys.reduce(self) { |a, e| a.fetch(e) }
+      end
     end
 
     module DigBang
       refine Hash do
-        def dig!(*keys)
-          Reading::Util.deep_fetch(self, keys)
-        end
+        include DeepFetch
       end
 
       refine Array do
-        def dig!(*keys)
-          Reading::Util.deep_fetch(self, keys)
-        end
+        include DeepFetch
       end
     end
   end
