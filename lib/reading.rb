cc9a7b93982398552991a70ae8f085b9953e5ee5
Felipe Vogel <fps.vogel@gmail.com>
2023-04-09 18:22:34 -0400

    Add handy architectural overview

--- lib/reading.rb
+++ lib/reading.rb
@@ -2,7 +2,19 @@ require_relative "reading/parsing/csv"
 require_relative "reading/item/time_length.rb"
 
 # The gem's public API. See https://github.com/fpsvogel/reading#usage
-
+#
+# Architectural overview:
+#
+#                 (CSV input)
+#                      |
+#                   ::parse         -------------> Item -----------> (Item output)
+#  Config,             |           /               / \
+#  errors.rb ----- Parsing::CSV ---       Item::View  Item::TimeLength
+#                     / \
+#      Parsing::Parser  Parsing::Transformer
+#             |                 |
+#      parsing/attributes/*  parsing/rows/*
+#
 module Reading
   # Parses a CSV file or string. See Parsing::CSV#initialize and #parse for details.
   def self.parse(...)
